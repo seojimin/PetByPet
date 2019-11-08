3860c9d7fcfa0d44c7f9402721359d8892465d21
seojiminca <seojiminca@gmail.com>
2019-11-08 21:13:22 +0900

    update server.js

--- server.js
+++ server.js
@@ -5,6 +5,25 @@ var dataServiceAuth = require('./data-service-auth.js');
 var bodyParser = require('body-parser');
 var express = require('express'); //Node.js web app framework 
 var app = express();
+var clientSessions = require('client-sessions');
+
+app.use(clientSessions({
+    cookieName: 'session',
+    secret: 'thisisaverylongandunguessablepassword',
+    duration: 5 * 60 * 1000,
+    activeDuration: 1000 * 60
+}));
+
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+});
+
+const ensureLogin = (req, res, next) => {
+    if (!req.session.user)
+        res.redirect("/login");
+    next();
+}
 
 function onHttpStart(){
     console.log('Express http server listening on ' + HTTP_PORT); //current 8080
@@ -30,7 +49,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-app.post("/customers/add", function(req, res) {
+app.post("/customers/add", ensureLogin, function(req, res) {
     console.log(req.body);
     dataService.addCustomer(req.body)
         .then(res.redirect("/customers"))
@@ -46,8 +65,10 @@ app.post("/customers/update", function (req, res){
 });
 
 dataService.initialize()
-    .then(() => app.listen(HTTP_PORT, onHttpStart))
-    .catch(() => console.log('some error occured')); 
+    .then(dataServiceAuth.initialize()
+        .then(() => app.listen(HTTP_PORT, onServerStart))
+        .catch('Unable to start auth'))
+    .catch(err => `unable to start server ${err}`)
 
 app.get("/customers/:value", function (req, res) {
     let number = req.params.value;
@@ -62,6 +83,43 @@ app.delete("/customers/delete/:cusNum", function (req, res){
     dataService.deleteCustomerByNum(cusNum)
         .then(() => res.redirect("/customers"))
         .catch(() => res.status(500).send("Unable to Remove Customer / Customer not found"));
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    req.body.userAgent = req.get('User-Agent');
+    dataServiceAuth.checkUser(req.body)
+        .then(user => {
+            req.session.user = {
+                userName: user.userName,
+                email: user.email,
+                loginHistory: user.loginHistory
+            }
+            res.redirect('/customers');
+        })
+        .catch(err => res.render('login', { errorMessage: err, userName: req.body.userName }));
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    dataServiceAuth.registerUser(req.body)
+        .then(() => res.render('register', { successMessage: "User created" }))
+        .catch(err => res.render('register', { errorMessage: err, userName: req.body.userName }))
+});
+
+app.get('/logout', (req, res) => {
+    req.session.reset();
+    res.redirect('/');
+});
+
+app.get('/userHistory', ensureLogin, (req, res) => {
+    res.render('userHistory')
 });
 
 
