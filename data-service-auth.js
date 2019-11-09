70108440dab0f0b0c610db7ed51c0be0e630e167
seojiminca <seojiminca@gmail.com>
2019-11-09 15:29:45 +0900

    Add: bcrypt

--- data-service-auth.js
+++ data-service-auth.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 var userSchema = new Schema({
     userName: {
@@ -30,16 +31,27 @@ module.exports.initialize = () =>
 
 module.exports.registerUser = userData =>
     new Promise((resolve, reject) => {
-        let newUser = new User(userData);
-        newUser.save(err => {
-            if (err) {
-                if (err.code == 11000) //mongo DB error code
-                    reject(`Username ${userData.userName} already taken.`);
 
-                reject(`"There was an error creating the user: ${err}`)
-            }
-            resolve();
-        });
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err)
+                reject(`There was an error encrypting the password ${err}`);
+            bcrypt.hash(userData.password, salt, (err, hash) => {
+                if (err)
+                    reject(`There was an error encrypting the password ${err}`);
+                userData.password = hash;
+
+                let newUser = new User(userData);
+                newUser.save(err => {
+                    if (err) {
+                        if (err.code == 11000) //mongo DB error code
+                            reject(`Username ${userData.userName} already taken.`);
+
+                        reject(`"There was an error creating the user: ${err}`)
+                    }
+                    resolve();
+                });
+            })
+        })
     });
 
 module.exports.checkUser = userData =>
@@ -49,23 +61,25 @@ module.exports.checkUser = userData =>
             .then(user => {
                 if (user === null)
                     reject(`Unable to find user: ${userData.userName}`);
-
-                if (userData.password != user.password)
-                    reject(`Incorrect Password for user: ${userData.userName}`)
-
-                user.loginHistory.push({
-                    dateTime: new Date().toString(),
-                    userAgent: userData.userAgent
-                });
-
-                User.update({ userName: userData.userName },
-                    { $set: { loginHistory: user.loginHistory } },
-                    { multi: false }, (err, raw) => {
-                        if (err) {
-                            reject(`There was an error verifying the user: ${err}`);
-                        }
-                        else
-                            resolve(user);
+                
+                bcrypt.compare(userData.password, user.password)
+                    .then(result => {
+                        if(!result)
+                        reject(`Incorrect Password for user: ${userData.userName}`);
+                        user.loginHistory.push({
+                            dateTime: new Date().toString(),
+                            userAgent: userData.userAgent
+                        });
+        
+                        User.update({ userName: userData.userName },
+                            { $set: { loginHistory: user.loginHistory } },
+                            { multi: false }, (err, raw) => {
+                                if (err) {
+                                    reject(`There was an error verifying the user: ${err}`);
+                                }
+                                else
+                                    resolve(user);
+                            })
                     })
             })
             .catch(err => reject(err));
